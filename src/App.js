e6a63c38f88ff4f03026e66432b161c4674e3f15
hadesyi <leesk55@gmail.com>
2020-06-28 07:58:53 +0900

    state 정의하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
+  add = () => {
+    this.state.count = 1;
+  };
+
+  minus = () => {
+    this.state.count = -1;
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
