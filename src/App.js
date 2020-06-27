99822c37f48867e9753e7a8036955a5d82c1bc79
hadesyi <leesk55@gmail.com>
2020-06-28 08:28:18 +0900

    클래스형 컴포넌트의 일생 알아보기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 class App extends React.Component {
+  constructor (props) {
+    super(props);
+    console.log('hello');
+  }
   state = {
     count: 0,
   };
 
   add = () => {
-    this.state.count = 1;
+    this.setState(current => ({
+      count: current.count + 1
+    }));
   };
 
   minus = () => {
-    this.state.count = -1;
+    this.setState(current => ({
+      count: current.count - 1
+    }));
   };
 
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world');
+  }
+
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
