ec058a884281e7d62e43a0cfb5ba59fbea42473b
hadesyi <leesk55@gmail.com>
2020-06-27 09:33:25 +0900

    03-4 컴포넌트 여러 개 사용해 보기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+function Movie() {
+  return <h1>I like potato</h1>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Movie />
     </div>
     );
 }
