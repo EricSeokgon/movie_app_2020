11c6f819806add5c8c5f1d0a8dd319655a7f4872
hadesyi <leesk55@gmail.com>
2020-06-28 16:59:02 +0900

    App.js 수정하기

---
+++ src/routes/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+
+}
+
+export default Home;
