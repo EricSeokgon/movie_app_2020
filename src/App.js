6ab9f28d80bb60f648ce426c399ac80b60b141a8
hadesyi <leesk55@gmail.com>
2020-06-28 15:10:54 +0900

    Movie 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         data: { movies },
       },
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    this.setState({ movies });
+    this.setState({ movies, isLoading: false });
   }
 
   componentDidMount() {
