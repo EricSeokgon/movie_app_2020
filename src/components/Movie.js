ebc9caeb8d9452be853549489a91a88f03d709ba
hadesyi <leesk55@gmail.com>
2020-06-28 16:55:39 +0900

    08-1 react-router-dom 설치하기

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,10 +10,11 @@ function Movie({ title, year, summary, poster, genres }) {
                 <h3 class="movie__title" >{title}</h3>
                 <h5 className="movie__year">{year}</h5>
                 <ul className="movie__genres">
-                    {genres.map((genre) => {
-                        return <li className="movie__genre">{genre}</li>;
+                    {genres.map((genre, index) => {
+                        return <li key={index} className="movie__genre">{genre}</li>;
                     })}
                 </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
