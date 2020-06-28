7fda34b5c262f63a008ecd2975be4b1806b40476
hadesyi <leesk55@gmail.com>
2020-06-28 16:09:34 +0900

    CSS 추가

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, title, year, summary, poster }) {
+function Movie({ title, year, summary, poster }) {
     return (
         <div class="movie">
             <img src={poster} alt={title} title={title} />
             <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
+                <h3 class="movie__title" style={{ backgroundColor: 'red' }}>
+                    {title}
+                </h3>
                 <h5 class="movie__year">{year}</h5>
                 <p class="movie__summary">{summary}</p>
             </div>
@@ -15,7 +18,6 @@ function Movie({ id, title, year, summary, poster }) {
 }
 
 Movie.prototype = {
-    id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
