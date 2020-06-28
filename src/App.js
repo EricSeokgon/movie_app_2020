a68b20ffaa3efd5cdbe50c34b8a6c4ac73542156
hadesyi <leesk55@gmail.com>
2020-06-28 16:04:17 +0900

    영화 포스터 추가하기

--- src/App.js
+++ src/App.js
@@ -13,29 +13,33 @@ class App extends React.Component {
       },
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     this.setState({ movies, isLoading: false });
-  }
-
+  };
   componentDidMount() {
     this.getMovies();
   }
-
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? 'Loading...'
-          : movies.map((movie) => {
-            return <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-            />;
-          })}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+            </div>
+          )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -2,7 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Movie({ id, title, year, summary, poster }) {
-    return <h4>{title}</h4>;
+    return (
+        <div class="movie">
+            <img src={poster} alt={title} title={title} />
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.prototype = {
